ab66f424af28955082c46a32757b79f03749db77
Tommy18062001 <johantony63@gmail.com>
2021-09-18 17:53:32 +0300

    My Home Portal Assignment

---
+++ Home Portal/js/script.js
@@ -0,0 +1,8 @@
+let dateData = new Date();
+
+let lastUpdated =document.getElementById("myDate"); 
+let myYear = document.getElementById("myYear");
+
+lastUpdated.textContent = document.lastModified;
+myYear.textContent = dateData.getFullYear();
+
---
+++ sandbox/main1.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+
+/* Calculate the area with the value above */
+area = radius * radius * PI;
+console.log(area);
+
+radius = 4;
+console.log(radius);
+
+/*recalculate the area */
+area = radius * radius * PI;
+console.log(area);
