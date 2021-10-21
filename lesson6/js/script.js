64ae26a67eaa5a6b9d1aceb60b629bad3caac2c0
Tommy18062001 <johantony63@gmail.com>
2021-10-21 15:13:20 +0300

    Final update for lesson 6 assignment

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -29,19 +29,3 @@ if (theDay === 5) {
 else {
   document.querySelector("#banner").classList.toggle("hide-banner");
 }
-
-
-/* calculate the wind chill */
-const temp = parseFloat(document.getElementById("temp").textContent);
-const speed = parseFloat(document.getElementById("speed").textContent);
-
-let f = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-f = Math.round(f);
-// console.log(f);
-
-if (temp <= 50 && speed > 3) {
-  document.getElementById("windChill").textContent = f + "\xB0F";
-}
-else {
-  document.getElementById("windChill").textContent = "No Wind Chill Today"
-}
