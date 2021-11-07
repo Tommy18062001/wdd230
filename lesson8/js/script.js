2b46f5e87eb709577e55e837b5555e21189e15ca
tommy18062001 <johantony63@gmail.com>
2021-11-07 07:36:15 +0300

    Final update for the storm center page

---
+++ lesson8/js/script.js
@@ -0,0 +1,36 @@
+const dateObject = new Date();
+
+const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const theDay = dateObject.getDay();
+const today = dateObject.getDate();
+const month = dateObject.getMonth();
+const year = dateObject.getFullYear();
+
+/* update the year in the footer*/
+document.querySelector("#today").textContent =  weekDay[theDay] + ", " + today + " " + monthNames[month] + " " + year;
+
+/* This will change the class of our nav to collapse */
+function toggleMenu() {
+    document.getElementById("ourNav").classList.toggle("hide")
+}
+
+/* Work on the banner message that will display a message*/
+/* Friday = 5 */
+
+if (theDay === 5) {
+  document.querySelector("#banner").classList.toggle("show-banner");
+}
+
+else {
+  document.querySelector("#banner").classList.toggle("hide-banner");
+}
+
+/* Adjust severity */
+function updateValue(rating) {
+  document.getElementById("severity").innerHTML = rating;
+}
